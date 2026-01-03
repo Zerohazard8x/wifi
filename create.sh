49fcbaf677f90ef44976c5ad9a07022e7d3bff00
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2026-01-03 00:51:07 -0500

    overhaul

--- create.sh
+++ create.sh
@@ -1,116 +1,91 @@
 #!/bin/bash
 
-# Function to create a Wi-Fi profile XML file
-create_wifi_profile() {
-    local wifi_name="$1"  # Capture the first argument as the Wi-Fi name
-    local file_name="Wi-Fi-${wifi_name}.xml"  # Construct the file name
-    
-    # Use a heredoc to write the XML content to the file
-    cat > "$file_name" <<EOF
-<?xml version="1.0"?>
-<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-    <name>$wifi_name</name>
-    <SSIDConfig>
-        <SSID>
-            <name>$wifi_name</name>
-        </SSID>
-    </SSIDConfig>
-    <connectionType>ESS</connectionType>
-    <connectionMode>auto</connectionMode>
-    <MSM>
-        <security>
-            <authEncryption>
-                <authentication>open</authentication>
-                <encryption>none</encryption>
-                <useOneX>false</useOneX>
-            </authEncryption>
-        </security>
-    </MSM>
-    <MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3">
-        <enableRandomization>true</enableRandomization>
-    </MacRandomization>
-</WLANProfile>
-EOF
-    
-    echo "'$file_name' created"
-}
-
 # android
-create_wifi_profile() {
+import_wifi_profile() {
     # adb shell "am start -n com.steinwurf.adbjoinwifi/.MainActivity -e ssid $ssid" # https://github.com/steinwurf/adb-join-wifi
     adb shell "am start -n com.adbwifisettingsmanager/.WifiSettingsManagerActivity --esn newConnection -e ssid $(printf '%q' "$1")" # https://github.com/pr4bh4sh/adb-wifi-setting-manager
 }
 
-create_wifi_profile "WiFi_Chefexpress"
-create_wifi_profile "WiFi Frecciarossa"
-create_wifi_profile "WiFi Frecciargento"
-create_wifi_profile "WendysWifi"
-create_wifi_profile "Wawa WiFi"
-create_wifi_profile "Walmartwifi"
-create_wifi_profile "Telstra"
-create_wifi_profile "Taronga Guest WiFi"
-create_wifi_profile "Sydney Tower - Guest"
-create_wifi_profile "SWP_Lounges_Wifi"
-create_wifi_profile "Starhotels"
-create_wifi_profile "Starbucks WiFi"
-create_wifi_profile "Smashburger Guest"
-create_wifi_profile "skynetglobal"
-create_wifi_profile "Shangri-La"
-create_wifi_profile "Scenic World Free Wifi"
-create_wifi_profile "RhB-FREE"
-create_wifi_profile "QF-Guest"
-create_wifi_profile "Portaleregionale"
-create_wifi_profile "PAL_Wi-Fi"
-create_wifi_profile "OperaHouse_FreeWiFi"
-create_wifi_profile "OnAir"
-create_wifi_profile "Olive Garden Guest"
-create_wifi_profile "My Autogrill WI-FI"
-create_wifi_profile "merz-gast"
-create_wifi_profile "Merlin_Guest"
-create_wifi_profile "McDonald's Free WiFi"
-create_wifi_profile "Max's WiFi"
-create_wifi_profile "Maccas Free Wi-fi"
-create_wifi_profile "KFC Free Wifi"
-create_wifi_profile "InfoTrainment"
-create_wifi_profile "IBIS"
-create_wifi_profile "Hungry Jack's FREE WiFi"
-create_wifi_profile "H&M Free WiFi"
-create_wifi_profile "Grill'd Guest"
-create_wifi_profile "Glowworm Caves Guest"
-create_wifi_profile "FreeWiFi@SYD"
-create_wifi_profile "FreeWiFi-NARITA(2.4G)"
-create_wifi_profile "FreeWiFi-NARITA"
-create_wifi_profile "FreeWiFi-NARITA-main(5G)"
-create_wifi_profile "FreeMSGWIFI"
-create_wifi_profile "Free_WiFi_GVA"
-create_wifi_profile "Free WiFi"
-create_wifi_profile "Free Telstra Wi-Fi"
-create_wifi_profile "Free Skyline Internet"
-create_wifi_profile "Fountains"
-create_wifi_profile "EmiliaRomagnaWiFi wifiprivacy.it"
-create_wifi_profile "Edelweiss"
-create_wifi_profile "DIHDXB"
-create_wifi_profile "digitalelpaso"
-create_wifi_profile "CP WIFI"
-create_wifi_profile "ConvergeFreeWifi_NAIAT3_5G"
-create_wifi_profile "Comune di Tirano WiFi Free"
-create_wifi_profile "ClientScape"
-create_wifi_profile "CircuitLaneFreeWIFI4ALL"
-create_wifi_profile "CBP_GUEST"
-create_wifi_profile "BoldynWiFi"
-create_wifi_profile "Boingo Hotspot"
-create_wifi_profile "Auckland Airport"
-create_wifi_profile "Apple Store"
-create_wifi_profile "Apple Demo"
-create_wifi_profile "ACCOR"
-create_wifi_profile "#Wifi@Jewel"
-create_wifi_profile "#Guest WiFi"
-create_wifi_profile "#DXB Free WiFi"
-create_wifi_profile "@WiFi UAE from du"
-create_wifi_profile "@EVIAFreeWiFi"
-create_wifi_profile "@Dubai Mall by EMAAR"
-create_wifi_profile "_WorldSquareFreeWifi_"
-create_wifi_profile "_FREE Smart WiFi @NAIA3"
-create_wifi_profile "_Free JFK WiFi"
-create_wifi_profile "_Free EWR Wifi"
-create_wifi_profile "_DarlingSquareFreeWifi_"
+import_wifi_profile "WiFi_Chefexpress"
+import_wifi_profile "WiFi Frecciarossa"
+import_wifi_profile "WiFi Frecciargento"
+import_wifi_profile "WendysWifi"
+import_wifi_profile "Wawa WiFi"
+import_wifi_profile "Walmartwifi"
+import_wifi_profile "Telstra"
+import_wifi_profile "Taronga Guest WiFi"
+import_wifi_profile "Sydney Tower - Guest"
+import_wifi_profile "SWP_Lounges_Wifi"
+import_wifi_profile "Starhotels"
+import_wifi_profile "Starbucks WiFi"
+import_wifi_profile "SolenadFreeWIFI4ALL"
+import_wifi_profile "Smashburger Guest"
+import_wifi_profile "skynetglobal"
+import_wifi_profile "Shangri-La"
+import_wifi_profile "Scenic World Free Wifi"
+import_wifi_profile "Rydges World Square"
+import_wifi_profile "RhB-FREE"
+import_wifi_profile "QF-Guest"
+import_wifi_profile "Portaleregionale"
+import_wifi_profile "PAL_Wi-Fi"
+import_wifi_profile "OperaHouse_FreeWiFi"
+import_wifi_profile "OnAir"
+import_wifi_profile "Olive Garden Guest"
+import_wifi_profile "My Autogrill WI-FI"
+import_wifi_profile "merz-gast"
+import_wifi_profile "Merlin_Guest"
+import_wifi_profile "McDonald's Free WiFi"
+import_wifi_profile "Max's WiFi"
+import_wifi_profile "Maccas Free Wi-fi"
+import_wifi_profile "KFC Free Wifi"
+import_wifi_profile "InfoTrainment"
+import_wifi_profile "IBIS"
+import_wifi_profile "Hungry Jack's FREE WiFi"
+import_wifi_profile "hideaway "
+import_wifi_profile "H&M Free WiFi"
+import_wifi_profile "Grill'd Guest"
+import_wifi_profile "Glowworm Caves Guest"
+import_wifi_profile "FreeWiFi@SYD"
+import_wifi_profile "FreeWiFi-NARITA(2.4G)"
+import_wifi_profile "FreeWiFi-NARITA"
+import_wifi_profile "FreeWiFi-NARITA-main(5G)"
+import_wifi_profile "FreeMSGWIFI"
+import_wifi_profile "Free_WiFi_GVA"
+import_wifi_profile "Free WiFi"
+import_wifi_profile "Free Telstra Wi-Fi"
+import_wifi_profile "Free Skyline Internet"
+import_wifi_profile "Fountains"
+import_wifi_profile "EmiliaRomagnaWiFi wifiprivacy.it"
+import_wifi_profile "Edelweiss"
+import_wifi_profile "DIHDXB"
+import_wifi_profile "digitalelpaso"
+import_wifi_profile "CP WIFI"
+import_wifi_profile "ConvergeFreeWifi_NAIAT3_5G"
+import_wifi_profile "Comune di Tirano WiFi Free"
+import_wifi_profile "ClientScape"
+import_wifi_profile "CircuitLaneFreeWIFI4ALL"
+import_wifi_profile "CBP_GUEST"
+import_wifi_profile "BoldynWiFi"
+import_wifi_profile "Boingo Hotspot"
+import_wifi_profile "Auckland Airport"
+import_wifi_profile "Atrium"
+import_wifi_profile "Apple Store"
+import_wifi_profile "Apple Demo"
+import_wifi_profile "Animo Student"
+import_wifi_profile "Animo Connect"
+import_wifi_profile "ACCOR"
+import_wifi_profile "#Wifi@Jewel"
+import_wifi_profile "#WiFi@Changi"
+import_wifi_profile "#Guest WiFi"
+import_wifi_profile "#GigaSmartWiFi"
+import_wifi_profile "#DXB Free WiFi"
+import_wifi_profile "@WiFi UAE from du"
+import_wifi_profile "@SOMOFreeWiFi"
+import_wifi_profile "@NAIAT3FreeWIFI4ALL"
+import_wifi_profile "@EVIAFreeWiFi"
+import_wifi_profile "@Dubai Mall by EMAAR"
+import_wifi_profile "_WorldSquareFreeWifi_"
+import_wifi_profile "_FREE Smart WiFi @NAIA3"
+import_wifi_profile "_Free JFK WiFi"
+import_wifi_profile "_Free EWR Wifi"
+import_wifi_profile "_DarlingSquareFreeWifi_
--- import.ps1
+++ import.ps1
@@ -1,19 +1,132 @@
-# Import all WLAN XML-files from specified directory
-$XmlDirectory = "."
+Set-StrictMode -Version Latest
 
-# Get all XML files in the directory
-$XmlFiles = Get-ChildItem -Path $XmlDirectory -Filter "*.xml"
+function Add-WifiNetwork {
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Ssid
+    )
 
-# Loop through each XML file
-foreach ($File in $XmlFiles) {
-    # Construct the full path for each file
-    $FilePath = Join-Path -Path $XmlDirectory -ChildPath $File.Name
+    # Mirror create.sh: file name pattern "Wi-Fi-<ssid>.xml"
+    # (but make it filesystem-safe so SSIDs like "A/B" won't break file creation)
+    $invalid = [IO.Path]::GetInvalidFileNameChars()
+    $safeSsidForFile = ($Ssid.ToCharArray() | ForEach-Object { if ($invalid -contains $_) { '_' } else { $_ } }) -join ''
+    $xmlFile = "Wi-Fi-$safeSsidForFile.xml"
 
-    # Try to add the WLAN profile using netsh
-    try {
-        $output = netsh wlan add profile filename="$FilePath"
-        Write-Verbose "Added profile for ${FilePath}: $output"
-    } catch {
-        Write-Warning "Failed to add profile for ${FilePath}: $_"
-    }
+    # Mirror create.sh: open network profile + MAC randomization block
+    $escapedSsid = [System.Security.SecurityElement]::Escape($Ssid)
+    $xml = @"
+<?xml version="1.0"?>
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+    <name>$escapedSsid</name>
+    <SSIDConfig>
+        <SSID>
+            <name>$escapedSsid</name>
+        </SSID>
+    </SSIDConfig>
+    <connectionType>ESS</connectionType>
+    <connectionMode>auto</connectionMode>
+    <MSM>
+        <security>
+            <authEncryption>
+                <authentication>open</authentication>
+                <encryption>none</encryption>
+                <useOneX>false</useOneX>
+            </authEncryption>
+        </security>
+    </MSM>
+    <MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3">
+        <enableRandomization>true</enableRandomization>
+    </MacRandomization>
+</WLANProfile>
+"@
+
+    $xml | Out-File -FilePath $xmlFile -Encoding UTF8 -Force
+
+    # Mirror import.ps1: import using netsh
+    netsh wlan add profile filename="$xmlFile"
 }
+
+# Example:
+Add-WifiNetwork "WiFi_Chefexpress"
+Add-WifiNetwork "WiFi Frecciarossa"
+Add-WifiNetwork "WiFi Frecciargento"
+Add-WifiNetwork "WendysWifi"
+Add-WifiNetwork "Wawa WiFi"
+Add-WifiNetwork "Walmartwifi"
+Add-WifiNetwork "Telstra"
+Add-WifiNetwork "Taronga Guest WiFi"
+Add-WifiNetwork "Sydney Tower - Guest"
+Add-WifiNetwork "SWP_Lounges_Wifi"
+Add-WifiNetwork "Starhotels"
+Add-WifiNetwork "Starbucks WiFi"
+Add-WifiNetwork "SolenadFreeWIFI4ALL"
+Add-WifiNetwork "Smashburger Guest"
+Add-WifiNetwork "skynetglobal"
+Add-WifiNetwork "Shangri-La"
+Add-WifiNetwork "Scenic World Free Wifi"
+Add-WifiNetwork "Rydges World Square"
+Add-WifiNetwork "RhB-FREE"
+Add-WifiNetwork "QF-Guest"
+Add-WifiNetwork "Portaleregionale"
+Add-WifiNetwork "PAL_Wi-Fi"
+Add-WifiNetwork "OperaHouse_FreeWiFi"
+Add-WifiNetwork "OnAir"
+Add-WifiNetwork "Olive Garden Guest"
+Add-WifiNetwork "My Autogrill WI-FI"
+Add-WifiNetwork "merz-gast"
+Add-WifiNetwork "Merlin_Guest"
+Add-WifiNetwork "McDonald's Free WiFi"
+Add-WifiNetwork "Max's WiFi"
+Add-WifiNetwork "Maccas Free Wi-fi"
+Add-WifiNetwork "KFC Free Wifi"
+Add-WifiNetwork "InfoTrainment"
+Add-WifiNetwork "IBIS"
+Add-WifiNetwork "Hungry Jack's FREE WiFi"
+Add-WifiNetwork "hideaway "
+Add-WifiNetwork "H&M Free WiFi"
+Add-WifiNetwork "Grill'd Guest"
+Add-WifiNetwork "Glowworm Caves Guest"
+Add-WifiNetwork "FreeWiFi@SYD"
+Add-WifiNetwork "FreeWiFi-NARITA(2.4G)"
+Add-WifiNetwork "FreeWiFi-NARITA"
+Add-WifiNetwork "FreeWiFi-NARITA-main(5G)"
+Add-WifiNetwork "FreeMSGWIFI"
+Add-WifiNetwork "Free_WiFi_GVA"
+Add-WifiNetwork "Free WiFi"
+Add-WifiNetwork "Free Telstra Wi-Fi"
+Add-WifiNetwork "Free Skyline Internet"
+Add-WifiNetwork "Fountains"
+Add-WifiNetwork "EmiliaRomagnaWiFi wifiprivacy.it"
+Add-WifiNetwork "Edelweiss"
+Add-WifiNetwork "DIHDXB"
+Add-WifiNetwork "digitalelpaso"
+Add-WifiNetwork "CP WIFI"
+Add-WifiNetwork "ConvergeFreeWifi_NAIAT3_5G"
+Add-WifiNetwork "Comune di Tirano WiFi Free"
+Add-WifiNetwork "ClientScape"
+Add-WifiNetwork "CircuitLaneFreeWIFI4ALL"
+Add-WifiNetwork "CBP_GUEST"
+Add-WifiNetwork "BoldynWiFi"
+Add-WifiNetwork "Boingo Hotspot"
+Add-WifiNetwork "Auckland Airport"
+Add-WifiNetwork "Atrium"
+Add-WifiNetwork "Apple Store"
+Add-WifiNetwork "Apple Demo"
+Add-WifiNetwork "Animo Student"
+Add-WifiNetwork "Animo Connect"
+Add-WifiNetwork "ACCOR"
+Add-WifiNetwork "#Wifi@Jewel"
+Add-WifiNetwork "#WiFi@Changi"
+Add-WifiNetwork "#Guest WiFi"
+Add-WifiNetwork "#GigaSmartWiFi"
+Add-WifiNetwork "#DXB Free WiFi"
+Add-WifiNetwork "@WiFi UAE from du"
+Add-WifiNetwork "@SOMOFreeWiFi"
+Add-WifiNetwork "@NAIAT3FreeWIFI4ALL"
+Add-WifiNetwork "@EVIAFreeWiFi"
+Add-WifiNetwork "@Dubai Mall by EMAAR"
+Add-WifiNetwork "_WorldSquareFreeWifi_"
+Add-WifiNetwork "_FREE Smart WiFi @NAIA3"
+Add-WifiNetwork "_Free JFK WiFi"
+Add-WifiNetwork "_Free EWR Wifi"
+Add-WifiNetwork "_DarlingSquareFreeWifi_
